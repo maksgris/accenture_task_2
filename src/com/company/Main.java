168626d03dfcbf3c3cca834a3cb2b7dc5e714f7a
Maksim Grishunin <maksgris@gmail.com>
2022-03-22 16:08:37 +0200

    Added first and second tasks solutions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
+import java.util.*;
 
 public class Main {
-    
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // First exercise
+
+        System.out.println("Please enter first number: ");
+        int a = input.nextInt();
+        System.out.println("Please enter second number: ");
+        int b = input.nextInt();
+
+        System.out.println("Is " + a + " equal to " + b + " ? - " + (a==b));
+        System.out.println("Is " + a + " less than " + b + " ? - " + (a<b));
+        System.out.println("Is " + a + " less or equal to " + b + " ? - " + (a<=b));
+        System.out.println("Is " + a + " greater than " + b + " ? - " + (a>b));
+        System.out.println("Is " + a + " greater or equal to " + b + " ? - " + (a>=b));
+
+        // Second exercise
+
+        System.out.println("Please enter first boolean value: ");
+        boolean b1 = input.nextBoolean();
+        System.out.println("Please enter first boolean value: ");
+        boolean b2 = input.nextBoolean();
+        System.out.println("Is b1 and b2 equal ? - " + (b1==b2));
+
+        
 
     }
 }
